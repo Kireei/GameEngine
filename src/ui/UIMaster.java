19a5ec7d3b0f7f7ae336971ae2dd6423086f73a6
Kireei <bjerned@gmail.com>
2019-04-06 21:06:23 +0200

    qasdf

--- src/ui/UIMaster.java
+++ src/ui/UIMaster.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector2f;
 
+import fontMeshCreator.GUIText;
 import fontRendering.TextMaster;
 import renderEngine.MasterRenderer;
 
@@ -16,10 +17,13 @@ public class UIMaster {
 	private static String testText = new String();
 	private static List<String> textInput = new ArrayList<String>();
 	
+	private static GUIText test = new GUIText(Long.toString(System.currentTimeMillis()), 1, UIHandler.font, new Vector2f(1,0), 50, false, false);
+	
 	public static UIElement activeText;
 	
 	public static void init() {
 		window = UIHandler.createWindow(new Vector2f(5,15));
+		TextMaster.loadText(test);
 		UIHandler.openWindow(window);
 		UIHandler.closeWindow(window);
 		
@@ -46,6 +50,7 @@ public class UIMaster {
 		} else if(uiState == UIState.TEXT_INPUT) {
 			getTextInput();
 		}
+		updateText();
 		for(UIElement uie: UIMaster.uies) {
 			if(uie.isActive()) {
 				uie.checkMouse();
@@ -72,9 +77,22 @@ public class UIMaster {
 				
 			}
 		}
-		activeText.createTitle(Long.toString(System.currentTimeMillis()), 1, new Vector2f(1,1));
-		activeText.getTexts().get(0).setColour(0, 0, 0);
-		TextMaster.loadText(activeText.getTexts().get(0));
+		
 		//System.out.println(text);
+	}
+	private static void updateText() {
+		for(GUIText text : window.get(1).getSliders().get(0)[2].getTexts()) {
+			TextMaster.removeText(text);
+		}
+		//TextMaster.removeText((window.get(1).getSliders().get(0)[2].getTexts().get(0)));
+		
+		//test = new GUIText(Integer.toString(SliderFunctions.planetResolution), 1, UIHandler.font, new Vector2f(1,0), 50, false, false);
+		window.get(1).getSliders().get(0)[2].getTexts().clear();
+		window.get(1).getSliders().get(0)[2].createTitle(Integer.toString(SliderFunctions.planetResolution), 1f, new Vector2f(0.01f,0.5f*0.028f));
+		
+		for(GUIText text : window.get(1).getSliders().get(0)[2].getTexts()) {
+			TextMaster.loadText(text);
+		}
+		//TextMaster.loadText(window.get(1).getSliders().get(0)[2].getTexts().get(0));
 	}
 }
